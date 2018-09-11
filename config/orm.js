469353c81695cf24e39042a26283effdec1eef0f
Nano Bacano <ricardo.bentin@gmail.com>
2018-09-11 09:03:38 -0400

    small change to js

--- config/orm.js
+++ config/orm.js
@@ -29,8 +29,6 @@ function objToSql(ob) {
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-      // e.g. {sleepy: true} => ["sleepy=true"]
       arr.push(key + "=" + value);
     }
   }
